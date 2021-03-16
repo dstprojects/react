59e51e2e0e50e2703de9f9b132a9cadcbff5869d
Alexis Guerrero <alexu.gro@gmail.com>
2021-03-15 20:46:40 -0600

    bandNames terminado

---
+++ react-sockets/03-bandnames/bandnames/src/hooks/useSocket.js
@@ -0,0 +1,42 @@
+import { useMemo, useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+
+export const useSocket = ( serverPath ) => {
+
+    const socket = useMemo(() => io.connect( serverPath, {
+        transports: ['websocket']
+    }), [ serverPath ] );
+    const [online, setOnline] = useState(false);
+
+    useEffect(() => {
+
+        setOnline( socket.connected );
+    
+    }, [socket])
+    
+    useEffect(() => {
+    
+        socket.on('connect', () => {
+          setOnline(true);
+        })
+    
+        return () => {
+          socket.disconnect();
+        } 
+          
+    
+    }, [socket])
+    
+    useEffect(() => {
+        socket.on('disconnect', () => {
+          setOnline(false);
+        })
+    }, [socket])
+
+    return {
+        socket,
+        online
+    }
+
+}
