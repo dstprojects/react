d49418317e95d45b220f6be7e730501746df1204
Alexis Guerrero <alexu.gro@gmail.com>
2021-04-05 21:55:41 -0500

    fin seccion 11

---
+++ react-sockets/06-chat/chat-app/src/pages/ChatPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ChatSelect } from '../components/ChatSelect'
+import { InboxPeople } from '../components/InboxPeople'
+import { Messages } from '../components/Messages'
+
+import '../css/chat.css'
+
+export const ChatPage = () => {
+    return (
+        <div className="messaging">
+            <div className="inbox_msg">
+
+                <InboxPeople />
+
+                {
+                    (false) 
+                        ? <Messages />
+                        : <ChatSelect />
+                }
+
+            </div>
+
+
+        </div>
+    )
+}
